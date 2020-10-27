3accca9c53666386f2569c2422ab988d185f8ca4
BoberPro <34961963+BoberPro@users.noreply.github.com>
2020-10-27 10:43:45 +0500

    Revert "Revert "Merge pull request #8 from artempetrushko/main""
    This reverts commit 42689092e74a1905b5d4fdbd75fd89e646d72f76.

--- BBC/Assets/Scripts/Enviroment/InformationGivers/InformationGiver2Behaviour.cs
+++ BBC/Assets/Scripts/Enviroment/InformationGivers/InformationGiver2Behaviour.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestGiver1Behaviour : MonoBehaviour
+public class InformationGiver2Behaviour : MonoBehaviour
 {
     GameObject button;
 
     void Start()
     {
-        button = GameObject.Find("TaskButton_1");
+        button = GameObject.Find("InformationButton_2");
         button.SetActive(false);
     }
 

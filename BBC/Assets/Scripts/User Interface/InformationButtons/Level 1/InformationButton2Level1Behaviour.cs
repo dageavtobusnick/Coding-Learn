ccd9ac70c21626d6d750bc802d5dcb431e914949
artempetrushko <64064252+artempetrushko@users.noreply.github.com>
2020-11-07 22:53:22 +0500

    Добавлены задания по теме Первое знакомство с С#
    - Добавлены задания по первой теме;
    - Добавлена теория по первой теме.

---
+++ BBC/Assets/Scripts/User Interface/InformationButtons/Level 1/InformationButton2Level1Behaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationButton2Level1Behaviour : MonoBehaviour
+{
+    GameObject player;
+    InputField theoryField;
+    string information = @"
+Точкой входа в программу является метод Main. При создании проекта создается следующий метод Main:
+using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		//Здесь помещаются инструкции
+        }
+    }
+}
+Пока что опустим весь код, что находится за пределами метода Main, позже мы его разберем. Как только программа запустится, она начнется с выполнения метода Main. 
+По умолчанию метод Main расположен внутри класса Program. Название может быть любым, но метод Main является обязательной частью консольного приложения.";
+
+
+
+    public void ShowInformation()
+    {
+        player.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+        theoryField.transform.position = theoryField.GetComponent<TheoryFieldBehaviour>().TurnOnPosition;
+        theoryField.text = information;
+    }
+
+    private void Start()
+    {
+        player = GameObject.Find("Snowman");
+        theoryField = GameObject.Find("TheoryField").GetComponent<InputField>();
+    }
+}
---
+++ BBC/Assets/Scripts/User Interface/InformationButtons/Level 1/InformationButton4Level1Behaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationButton4Level1Behaviour : MonoBehaviour
+{
+    GameObject player;
+    InputField theoryField;
+    string information = @"                                                             Переменные
+    Для хранения данных в программе используется переменные. Переменная – именованная область памяти, в которой хранится значение определенного типа
+В С# есть своя система типов данных.
+У переменной есть тип и значение
+Тип – определяет внутреннее значение переменной(целое число, дробное число, текст и т.д.)
+Значение – определяет содержимое переменной(конкретное число или строка и т.д.)
+Перед использованием переменной ее нужно объявить
+тип имя_переменной;
+Имя может содержать любые цифры, буквы и символ подчеркивания, при этом первый символ в имени должен быть буквой или символом подчеркивания
+В имени не должно быть знаков пунктуации и пробелов
+Имя не может быть ключевым словом языка C#.
+Имя может быть любым, но следует давать осмысленные имена, чтобы любой, кто захочет прочитать код, мог понять для чего именно эта переменная объявлена.
+
+int: хранит целое число от и занимает 4 байта (int a);
+double: хранит дробное число с плавающей точкой и занимает 8 байт (double d);
+char: хранит одиночный символ в кодировке Unicode и занимает 2 байта (char c);
+string: хранит набор символов Unicode (string s);
+bool: хранит логическое значение true или false (правда или ложь, 1 или 0) (bool b);";
+
+    public void ShowInformation()
+    {
+        player.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+        theoryField.transform.position = theoryField.GetComponent<TheoryFieldBehaviour>().TurnOnPosition;
+        theoryField.text = information;
+    }
+
+    private void Start()
+    {
+        player = GameObject.Find("Snowman");
+        theoryField = GameObject.Find("TheoryField").GetComponent<InputField>();
+    }
+}

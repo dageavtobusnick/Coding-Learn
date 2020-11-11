a74ad9da0470f8ce7c1e42e023acb90baf86741b
artempetrushko <64064252+artempetrushko@users.noreply.github.com>
2020-11-11 18:25:22 +0500

    Добавлен экспериментальный уровень с новыми механиками
    Добавлен экспериментальный уровень с новыми механиками:
    - теперь у игрока есть справочник, куда он может обращаться за теорией по изученным темам (кнопка со знаком вопроса);
    - игрок может получить часть теории у старика-мудреца в обмен на рубины (1 рубин = 1 часть теории). Таким образом, части теории будут доступны последовательно;
    - Рубины можно найти на уровне, они должны быть запрятаны (пока они на виду));

---
+++ BBC/Assets/Scripts/TestScripts/OldmanButtonBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldmanButtonBehaviour : MonoBehaviour
+{
+    private GameObject player;
+    private GameObject exchangePanel;
+
+    public void ShowExchangePanel()
+    {
+        player.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+        exchangePanel.transform.position = exchangePanel.GetComponent<ExchangePanelBehaviour>().TurnOnPosition;
+    }
+
+    private void Start()
+    {
+        player = GameObject.Find("Snowman");
+        exchangePanel = GameObject.Find("Panel_RubyToTheoryExchange");
+    }
+}
---
+++ BBC/Assets/Scripts/TestScripts/TheoryBook/Main Themes/Theme3ButtonBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Theme3ButtonBehaviour : MonoBehaviour
+{
+    private GameObject mainThemes;
+    private GameObject subThemes;
+
+    public void ShowSubThemes()
+    {
+        mainThemes.transform.position = mainThemes.GetComponent<ThemePanelsBehaviour>().TurnOffPosition;
+        subThemes.transform.position = subThemes.GetComponent<ThemePanelsBehaviour>().TurnOnPosition;
+    }
+
+    private void Start()
+    {
+        mainThemes = GameObject.Find("Panel_MainThemes");
+        subThemes = GameObject.Find("Panel_Theme3");
+    }
+}

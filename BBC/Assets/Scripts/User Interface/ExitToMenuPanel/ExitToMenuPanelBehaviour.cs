48a142c5142c253e217a3c4151d620f7d8fa2d86
artempetrushko <64064252+artempetrushko@users.noreply.github.com>
2020-12-15 16:54:39 +0500

    Доработана кнопка выхода в меню
    - Кнопка выхода в настройки заменена на кнопку выхода в меню
    - Перед выходом появляется предупреждающее окно

---
+++ BBC/Assets/Scripts/User Interface/ExitToMenuPanel/ExitToMenuPanelBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitToMenuPanelBehaviour : MonoBehaviour
+{
+    public Vector3 TurnOnPosition;
+    public Vector3 TurnOffPosition;
+
+    void Start()
+    {
+        TurnOnPosition = gameObject.transform.position;
+        TurnOffPosition = GameObject.Find("UI_Collector").transform.position;
+        gameObject.transform.position = TurnOffPosition;
+    }
+}

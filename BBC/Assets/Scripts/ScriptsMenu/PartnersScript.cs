254cb8fb0316537701f261e8bd253b91e1aca068
BoberPro <34961963+BoberPro@users.noreply.github.com>
2021-01-11 21:21:31 +0500

    Добавление партнеров FiveRaccoons

---
+++ BBC/Assets/Scripts/ScriptsMenu/PartnersScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartnersScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void abc()
+    {
+        Application.OpenURL("http://13.59.215.174/FiveRaccoons/");
+    }
+}

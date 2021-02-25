974c43bf4e583e43d115815c9732224f01dc5378
Sergeydm <32673289+Sergeydm@users.noreply.github.com>
2021-02-25 20:12:44 +0500

    Добавлено небо на levelA, удалены все билд версии

--- BBC/builds_pre/UWB_x86LZ4HC/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_gc.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\Bober\Desktop\23122020\SonSyki\BBC\builds_pre\UWB_x86LZ4HC\Il2CppOutputProject\IL2CPP\libil2cpp\gc\BoehmGC.cpp"
-#include "C:\Users\Bober\Desktop\23122020\SonSyki\BBC\builds_pre\UWB_x86LZ4HC\Il2CppOutputProject\IL2CPP\libil2cpp\gc\GCHandle.cpp"
-#include "C:\Users\Bober\Desktop\23122020\SonSyki\BBC\builds_pre\UWB_x86LZ4HC\Il2CppOutputProject\IL2CPP\libil2cpp\gc\GarbageCollector.cpp"
-#include "C:\Users\Bober\Desktop\23122020\SonSyki\BBC\builds_pre\UWB_x86LZ4HC\Il2CppOutputProject\IL2CPP\libil2cpp\gc\NullGC.cpp"
-#include "C:\Users\Bober\Desktop\23122020\SonSyki\BBC\builds_pre\UWB_x86LZ4HC\Il2CppOutputProject\IL2CPP\libil2cpp\gc\WriteBarrier.cpp"
-#include "C:\Users\Bober\Desktop\23122020\SonSyki\BBC\builds_pre\UWB_x86LZ4HC\Il2CppOutputProject\IL2CPP\libil2cpp\gc\WriteBarrierValidation.cpp"

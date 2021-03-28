6fb48740869913e84075e37ab68edea5747d41ab
artempetrushko <64064252+artempetrushko@users.noreply.github.com>
2021-03-28 22:20:47 +0500

    Добавлены задания на обучающий и 1-й уровень, обновлён интерфейс

---
+++ BBC/Assets/Scripts/User Interface (new)/Common Scripts/GameData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public Camera currentSceneCamera;
+    public int currentTaskNumber;
+}
---
+++ BBC/Assets/Scripts/User Interface (new)/Common Scripts/RobotBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotBehaviour : MonoBehaviour
+{
+    private float moveSpeed = 0.05f;
+    private float rotateSpeed = 3f;
+    private Vector3 movement;
+    private Vector3 rotation;
+
+    void FixedUpdate()
+    {
+        var deltaX = Input.GetAxis("Vertical") * moveSpeed;
+        var deltaY = Input.GetAxis("Horizontal") * rotateSpeed;
+        rotation = new Vector3(0, deltaY, 0);
+        transform.Rotate(rotation);
+        switch (transform.rotation.y % 180)
+        {
+            case 0:
+                movement = new Vector3(0, 0, deltaX);
+                break;
+            case 90:
+                movement = new Vector3(deltaX, 0, 0);
+                break;
+            default:
+                movement = new Vector3(deltaX, 0, deltaX);
+                break;
+        }
+        transform.Translate(movement);
+    }
+}

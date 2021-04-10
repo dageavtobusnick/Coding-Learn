7dac03e796c62c659e6bb8f43fe9d778e05a98ac
artempetrushko <64064252+artempetrushko@users.noreply.github.com>
2021-04-10 21:15:17 +0500

    Добавлен 2 уровень с заданиями

--- BBC/Assets/Scripts/User Interface (new)/Common Scripts/GameData.cs
+++ BBC/Assets/Scripts/User Interface (new)/Common Scripts/GameData.cs
@@ -6,4 +6,9 @@ public class GameData : MonoBehaviour
 {
     public Camera currentSceneCamera;
     public int currentTaskNumber;
+
+    private void Start()
+    {
+        currentSceneCamera = GameObject.Find("SceneCamera_1").GetComponent<Camera>();
+    }
 }
--- BBC/Assets/Scripts/User Interface (new)/Common Scripts/RobotBehaviour.cs
+++ BBC/Assets/Scripts/User Interface (new)/Common Scripts/RobotBehaviour.cs
@@ -4,29 +4,14 @@ using UnityEngine;
 
 public class RobotBehaviour : MonoBehaviour
 {
-    private float moveSpeed = 0.05f;
+    private float moveSpeed = 3f;
     private float rotateSpeed = 3f;
-    private Vector3 movement;
-    private Vector3 rotation;
 
     void FixedUpdate()
     {
-        var deltaX = Input.GetAxis("Vertical") * moveSpeed;
-        var deltaY = Input.GetAxis("Horizontal") * rotateSpeed;
-        rotation = new Vector3(0, deltaY, 0);
-        transform.Rotate(rotation);
-        switch (transform.rotation.y % 180)
-        {
-            case 0:
-                movement = new Vector3(0, 0, deltaX);
-                break;
-            case 90:
-                movement = new Vector3(deltaX, 0, 0);
-                break;
-            default:
-                movement = new Vector3(deltaX, 0, deltaX);
-                break;
-        }
-        transform.Translate(movement);
+        var deltaX = Input.GetAxis("Horizontal");
+        var deltaZ = Input.GetAxis("Vertical");
+        transform.Rotate(0f, deltaX * rotateSpeed, 0f);
+        transform.Translate(new Vector3(0f, 0f, -deltaZ * moveSpeed * Time.deltaTime));   
     }
 }
